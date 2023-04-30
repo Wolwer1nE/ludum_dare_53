a0a162f5cf95a89c4f4eb15d027895b332e24184
Wolwer1ne <wolwerine@yandex.ru>
2023-04-30 20:03:46 +0300

    Build without gitkeep

---
+++ docs/gamedata/docs/gamedata/app/config.rb
@@ -0,0 +1,7 @@
+DEFAULT_LABEL_CONFIG = {
+  font: 'fonts/Dimbo Regular.ttf',
+  size_px:                 20,
+  r:                       0,
+  g:                       0,
+  b:                       0,
+  a:                       255}
